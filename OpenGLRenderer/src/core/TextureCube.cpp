2f8b59125a3e3033a4d0331092edc2b662406bc6
Nick Houghton <nhoughto5@gmail.com>
2021-01-24 21:24:10 -0800

    Fixed skymap

--- OpenGLRenderer/src/core/TextureCube.cpp
+++ OpenGLRenderer/src/core/TextureCube.cpp
@@ -21,7 +21,7 @@ void TextureCube::loadTexture(std::string src) {
         unsigned char* data = ImageLoader::loadImage((ASSET_FOLDER + src + faces[i]).c_str(), &width, &height, &nrChannels);
         if (data) {
             glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i,
-                0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data
+                0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data
             );
             ImageLoader::Clear(data);
         }
